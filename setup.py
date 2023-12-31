0a3986efc65c599e1ac1e2cecfa1ecc237184645
Pukar Sharma <sharma.pukar99@gmail.com>
2023-12-31 16:15:58 +0545

    setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+# import os
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    ''' 
+    This function returns the list of requirements.
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
+    name='MLProjects',
+    version='0.0.1',
+    author='Pukar Sharma',
+    author_email='sharma.pukar99@gmail.com',  
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+    # # Optional fields
+    # long_description=open('README.md').read() if os.path.exists('README.md') else '',
+    # url='https://github.com/Pukar99/MLProjects',  
+)
